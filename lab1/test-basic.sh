62938cfac26fd2dd6ff01e27f6984f10ce36a233
Christian Lao <clao@ucla.edu>
2012-01-31 18:44:13 -0800

    Added test file for real

---
+++ lab1/test-basic.sh
@@ -0,0 +1,29 @@
+#Test basic commands one-by-one
+
+#Test simple command
+echo Simple commands work.
+
+#Test AND/OR command
+touch ThisShouldDelete.txt && rm ThisShouldDelete.txt
+echo AND Commands work.
+
+echo No file should be created || touch ShouldNotExist.txt
+echo OR Commands work.
+
+#Test pipes
+echo Successful pipe prints makefile
+ls | grep f
+
+#Test I/O redirection
+echo contents of foo > foo.txt
+cat < foo.txt > bar.txt
+echo No diff statements should appear
+diff foo.txt bar.txt
+rm foo.txt
+rm bar.txt
+
+#Test Subshells
+#(echo Subshells work > subshellio.txt)
+#(cat subshellio.txt)
+#(rm subshellio.txt)
+
